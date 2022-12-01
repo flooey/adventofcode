4beeab77f0bb32bd7ffeaa8f25e391424e1567ab
Adam Vartanian <flooey@gmail.com>
2022-12-01 06:50:53 +0000

    2022 day 1

---
+++ 2022/day1/part1.cr
@@ -0,0 +1,17 @@
+elves = [] of Int32
+
+cur_elf = 0
+while line = gets
+    line = line.chomp
+    if line == ""
+        elves << cur_elf
+        cur_elf = 0
+    else
+        cur_elf += line.to_i
+    end
+end
+if cur_elf > 0
+    elves << cur_elf
+end
+
+puts elves.max
