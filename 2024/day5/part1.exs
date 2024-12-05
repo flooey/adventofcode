c83dbf3e8fce5819fc115a1aeaeaf2b8269c6076
Adam Vartanian <flooey@gmail.com>
2024-12-05 07:17:24 +0000

    2024 day 5

---
+++ 2024/day5/part1.exs
@@ -0,0 +1,31 @@
+rules =
+  IO.stream()
+  |> Stream.take_while(&(&1 != "\n"))
+  |> Enum.map(fn x ->
+    String.trim(x)
+    |> String.split("|")
+    |> Enum.map(&String.to_integer/1)
+    |> List.to_tuple()
+  end)
+  |> MapSet.new()
+
+pages =
+  IO.stream()
+  |> Enum.map(fn l ->
+    String.trim(l) |> String.split(",") |> Enum.map(&String.to_integer/1)
+  end)
+
+defmodule Day5 do
+  def pairs([_]), do: []
+  def pairs([head | tail]), do: (tail |> Enum.map(&{&1, head})) ++ Day5.pairs(tail)
+end
+
+IO.puts(
+  Enum.sum(
+    pages
+    |> Enum.filter(fn p ->
+      MapSet.size(Day5.pairs(p) |> MapSet.new() |> MapSet.intersection(rules)) == 0
+    end)
+    |> Enum.map(fn p -> List.to_tuple(p) |> then(&elem(&1, div(tuple_size(&1) - 1, 2))) end)
+  )
+)
