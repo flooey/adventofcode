3b1800f0d15b5f57e4e3202ec4a58e3970ad9d12
Adam Vartanian <flooey@gmail.com>
2025-12-04 07:19:27 +0000

    2025 day 4

---
+++ 2025/day4/part2.nim
@@ -0,0 +1,34 @@
+var line: string
+var lines: seq[string]
+
+while readline(stdin, line):
+    lines.add(line)
+
+proc neighbors(x: int, y: int, lines: seq[string]): int =
+    var total = 0
+    for i in (x-1)..(x+1):
+        for j in (y-1)..(y+1):
+            if i == x and j == y:
+                continue
+            if i < 0 or i >= lines.len:
+                continue
+            if j < 0 or j >= lines[i].len:
+                continue
+            if lines[i][j] == '@':
+                inc total
+        
+    return total
+
+var total_removed = 0
+var removed = true
+
+while removed:
+    removed = false
+    for i in 0..(lines.len - 1):
+        for j in 0..(lines[i].len - 1):
+            if lines[i][j] == '@' and neighbors(i, j, lines) < 4:
+                inc total_removed
+                removed = true
+                lines[i][j] = '.'
+
+echo total_removed
